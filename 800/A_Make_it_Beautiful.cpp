c6261743f18f5f99e34dd0555c12f3ed20d745d0
Naitik Gupta <206251886+naitik2043@users.noreply.github.com>
2026-01-19 23:01:51 +0530

    Add files via upload

---
+++ 800/A_Make_it_Beautiful.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin>>n;
+
+        vector<int>a(n);
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        if(a[0] == a[n-1]){
+            cout<<"No"<<endl;
+        }else{
+            cout<<"YEs"<<endl;
+            cout<<a[n-1]<<" ";
+            for(int i=0; i<n-1; i++){
+                cout<<a[i]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
